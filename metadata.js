d3cb4ec107abd19ef6ca8844020f7f514724696f
trapmoneyrai <97180669+trapmoneyrai@users.noreply.github.com>
2022-08-06 19:45:27 +0200

    correct script & options

--- metadata.js
+++ metadata.js
@@ -64,10 +64,17 @@ overlay.addEventListener('click', () => {
 
 // download button
 
-window.onload = function() {
-    document.getElementById("download-btn").addEventListener("click", ()=>{
-        const metadataFile = this.document.getElementById("display-metadata");
-        console.log(metadataFile)
-        html2pdf().from("display-metdata").save();
-    })
+window.onload = function () {
+	document.getElementById("download-btn").addEventListener("click", () => {
+		const metadataFile = this.document.getElementById("display-metadata");
+		console.log(metadataFile)
+		var opt = {
+			margin:       1,
+			filename:     'mymetadata.pdf',
+			image:        { type: 'jpeg', quality: 0.98 },
+			html2canvas:  { scale: 2 },
+			jsPDF:        { unit: 'in', format: 'letter', orientation: 'portrait' }
+		  };
+		html2pdf().from("display-metdata").set(opt).save();
+	})
 }
